7959b19a1378030e05049e16697b6192500ad0e8
Felipe <felipeems87@gmail.com>
2017-07-20 19:36:47 -0300

    Correção de bug na máscara de cpf. Ao copiar a máscara não era aplicada ao conteúdo do novo campo.

--- settran-portal/src/app/components/area-municipe/cadastro-dat/seu-veiculo/seu-veiculo.component.ts
+++ settran-portal/src/app/components/area-municipe/cadastro-dat/seu-veiculo/seu-veiculo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EDATService } from '../../../shared/services/e-dat.service';
 
+declare var $:any; // JQUERY
+
 @Component({
   selector: 'app-seu-veiculo',
   templateUrl: './seu-veiculo.component.html',
@@ -10,13 +12,31 @@ export class SeuVeiculoComponent implements OnInit {
 
   constructor(private edatService: EDATService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.setMasks();
+  }
+  
+  setMasks() {
+	$('.cep').mask('00000-000');
+	$('.phone').mask('0000-0000');
+    $('.phone_with_ddd').mask('(00) 00000-0000');
+	$('.cpf').mask('000.000.000-00', {reverse: true});
+  }
+  
+  resetCPFMask() {  
+	setTimeout(function() {
+		$('#docProprietario').unmask().mask('000.000.000-00', {reverse: true});
+	}, 1000);
+  }
   
   alteraPossuiVeiculo() {}
   
   setDadosProprietario() {
     this.edatService.eDAT.nomePropietario = this.edatService.eDAT.nomeMunicipe;
-    this.edatService.eDAT.docPropietario = this.edatService.eDAT.cpf;
+	console.log(this.edatService.eDAT.cpf);
+	let doc = this.edatService.eDAT.cpf.replace(/\D/g,'');
+	this.edatService.eDAT.docPropietario = doc;
+    this.resetCPFMask();
   }
 
 }
