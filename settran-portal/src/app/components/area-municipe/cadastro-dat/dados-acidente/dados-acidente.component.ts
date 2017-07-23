b20124bea505c48815d185ba7a4d39434e20d0ae
Felipe <felipeems87@gmail.com>
2017-07-23 11:08:20 -0300

    Adicionando jqueryui ao projeto para suporte de autocomplete.

--- settran-portal/src/app/components/area-municipe/cadastro-dat/dados-acidente/dados-acidente.component.ts
+++ settran-portal/src/app/components/area-municipe/cadastro-dat/dados-acidente/dados-acidente.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { EDATService } from '../../../shared/services/e-dat.service';
 
+declare var $:any; // JQUERY
+
 @Component({
   selector: 'app-dados-acidente',
   templateUrl: './dados-acidente.component.html',
-  styleUrls: ['./dados-acidente.component.css']
+  styleUrls: ['dados-acidente.component.css']
 })
 export class DadosAcidenteComponent implements OnInit {
 
   constructor(private edatService: EDATService) { }
 
   ngOnInit() {
+	this.configuraAutoComplete();
   }
 
   alteraTipoAcidente(tipoAcidente: string) {
@@ -19,5 +22,38 @@ export class DadosAcidenteComponent implements OnInit {
 
   alteraZona(zona: string) {
 	console.log('alteraZona -> '+ zona);
+  }
+  
+  configuraAutoComplete() {
+    $( function() {
+	
+		$( ".logradouro-auto-complete" ).autocomplete({
+		  source: function( request, response ) {
+			$.ajax( {
+			  url: "http://ec2-52-67-135-39.sa-east-1.compute.amazonaws.com:8080/wsedat/rest/logradouroservice/recuperalogradouropordescricao",
+			  dataType: "json",
+			  data: {
+				textologradouro: request.term
+			  },
+			  success: function( data ) {
+				//response( data );
+				response($.map(data, function(item) {
+                    return {
+                        label: item.tipoLogradouro+" "+item.nomeLogradouro,
+                        value: item.nomeLogradouro
+                    }
+                }))
+			  },
+			  beforeSend: function (xhr) { /* Authorization header */
+				xhr.setRequestHeader("Authorization", "e96b4ae0-e36a-648f-134f-44171c2dcb18");
+			  }
+			} );
+		  },
+		  minLength: 3,
+		  select: function( event, ui ) {
+			console.log( "Selected: " + ui );
+		  }
+		} );
+	} );
   }
 }
