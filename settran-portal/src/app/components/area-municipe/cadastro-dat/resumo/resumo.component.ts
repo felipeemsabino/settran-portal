f7549d5cd54d5ea46c1e269ccac48794c2c025f5
Felipe <felipeems87@gmail.com>
2017-07-25 17:42:20 -0300

    Tela resumo dat implementada.

--- settran-portal/src/app/components/area-municipe/cadastro-dat/resumo/resumo.component.ts
+++ settran-portal/src/app/components/area-municipe/cadastro-dat/resumo/resumo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EDATService } from '../../../shared/services/e-dat.service';
 
 @Component({
   selector: 'app-resumo',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private edatService: EDATService) { }
 
   ngOnInit() {
   }
