f13758d6fd39478dcb5d063524ada15c2e5c5941
Felipe Sabino <felipeems87@gmail.com>
2017-08-20 18:45:59 +0100

    Login e recuperar senha parcialmente implementados.

--- settran-portal/src/app/components/home/home.component.ts
+++ settran-portal/src/app/components/home/home.component.ts
@@ -1,27 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, Router }   from '@angular/router';
+import { LoginService } from './services/login.service';
+import { URLSearchParams } from '@angular/http';
+import { PopupControllerComponent } from '../shared/popup-controller/popup-controller.component';
+
+declare var $:any; // JQUERY
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[LoginService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private loginService: LoginService,
+              private popupController: PopupControllerComponent) { }
 
   ngOnInit() {
+
+    	setTimeout(function() {
+    	  $('.cpf').unmask().mask('000.000.000-00', {reverse: true});
+    	}, 500);
   }
 
-	registerCredentials = {user: '', pass: ''};
+	registerCredentials: any = {usuario: '', senha: ''};
+  esqueciSenha: any = {cpf:''};
+
+  validaCamposEsqueciSenha() {
+    if(this.esqueciSenha.cpf.length == 0 ) {
+      this.popupController.showPopupMessage("Aguarde!", "Por favor, insira seu CPF para prosseguir com a recuperação da senha.", true);
+      return false;
+    }
+    return true;
+  }
+
+  validaCamposLogin() {
+    if(this.registerCredentials.usuario.length == 0 || this.registerCredentials.senha.length == 0) {
+      this.popupController.showPopupMessage("Aguarde!", "Por favor, preencha os campos de usuário e senha.", true);
+      return false;
+    }
+    return true;
+  }
+
+  recuperarSenha() {
+    if(!this.validaCamposEsqueciSenha()){
+      return false;
+    }
+    let params: URLSearchParams = new URLSearchParams();
+
+    this.popupController.showPopupMessage("Aguarde!", "Recuperando sua senha.", false);
+    params = this.registerCredentials;
+    // Salva dado
+    this.loginService.recuperarSenha(params)
+                      .subscribe(
+                          result => {
+                            console.log(result);
+                            this.popupController.showPopupMessage("Aguarde!", "Sua senha foi recuperada com sucesso, verifique seu e-mail cadastrado para maiores informações.", true);
+
+                          }, //Bind to view
+                          err => {
+                            this.popupController.showPopupMessage("Atenção!",
+                            "Ocorreram erros ao recuperar sua senha! Por favor, tente novamente.", true);                              console.log(err);
+                          });
+  }
 
   entrar () {
-	if(this.registerCredentials.user === 'agente' && this.registerCredentials.user === 'agente')
-    {
+    /*if(this.registerCredentials.usuario === 'agente' && this.registerCredentials.senha === 'agente') {
       this.router.navigate(['/area-agente']);
-    } else if(this.registerCredentials.user === 'admin' && this.registerCredentials.user === 'admin')
-    {
+    } else if(this.registerCredentials.usuario === 'admin' && this.registerCredentials.senha === 'admin') {
       this.router.navigate(['/area-admin']);
+    }*/
+
+    if(!this.validaCamposLogin()){
+      return false;
     }
+    let params: URLSearchParams = new URLSearchParams();
+
+    this.popupController.showPopupMessage("Aguarde!", "Realizando login.", false);
+    params = this.registerCredentials;
+    // Salva dado
+    this.loginService.realizaLogin(params)
+                      .subscribe(
+                          result => {
+                            console.log(result);
+                          }, //Bind to view
+                          err => {
+                            this.popupController.showPopupMessage("Atenção!",
+                            "Ocorreram erros ao realizar o login! Por favor, tente novamente.", true);                              console.log(err);
+                          });
   }
 }
---
+++ settran-portal/src/app/components/home/services/login.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response, URLSearchParams, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
+@Injectable()
+export class LoginService {
+
+  constructor(private http: Http) { }
+
+  realizaLogin(params: URLSearchParams): Observable<any []> {
+
+    let header = new Headers();
+    header.append('Content-Type', 'application/json');
+    header.append('authorization', 'e96b4ae0-e36a-648f-134f-44171c2dcb18');
+
+    let options = new RequestOptions({ headers: header, search: params });
+
+    return this.http.get('http://ec2-52-67-135-39.sa-east-1.compute.amazonaws.com:8080/wsedat/rest/agente/login/', options)
+                     .map((res:Response) =>this.extractData(res))
+                     .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  recuperarSenha(params: URLSearchParams): Observable<any []> {
+
+    let header = new Headers();
+    header.append('Content-Type', 'application/json');
+    header.append('authorization', 'e96b4ae0-e36a-648f-134f-44171c2dcb18');
+
+    let options = new RequestOptions({ headers: header, search: params });
+
+    return this.http.get('http://ec2-52-67-135-39.sa-east-1.compute.amazonaws.com:8080/wsedat/rest/agente/listaagentes/', options)
+                     .map((res:Response) =>this.extractData(res))
+                     .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  extractData(res: Response) {
+    return res.json();
+  }
+
+}
